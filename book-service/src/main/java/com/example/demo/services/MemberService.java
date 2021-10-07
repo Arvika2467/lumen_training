ade67c4263f14bb5b6358438b2299fc3f8d31a3f
arvika <arvika2467@gmail.com>
2021-10-07 14:40:06 +0530

    Created a member service class to satisfy the requirements

---
+++ book-service/src/main/java/com/example/demo/services/MemberService.java
@@ -0,0 +1,14 @@
+package com.example.demo.services;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberService {
+
+	private int id;
+	private String name;
+}
